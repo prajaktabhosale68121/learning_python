c81093afbf7344508b36c6697abec1d585bf2cee
vijay <vijay777pawar@gmail.com>
2019-01-04 19:34:46 +0530

    practicing variable

---
+++ variable.py
@@ -0,0 +1,55 @@
+# Question : How to create variable
+name = 'prajakta'
+print(name)
+
+# Question : How to assign value to variable
+a='20'
+print(a)
+
+# Question : How to print variable
+a1='20'
+print(a1)
+
+# Question : How to contact a string variable and print
+a2='prajakta'
+b2='bhosale'
+c2= a2+b2
+print(c2)
+
+# Question : How to add two number variable and print
+e=10
+f=20
+g=e+f
+print(g)
+
+# Question : How to create array of string
+arr=['orange','apple','banana']
+print(arr)
+
+# Question : How to create array of (object/json/dictionary).That contains information of two people
+d=[ 
+    { 
+        'age':26 , 
+        'name' : 'prajakta', 
+        'address' : 'andheri' , 
+        'medals' : 7
+        },
+    { 
+        'age':30 , 
+        'name' : 'vijay', 
+        'address' : 'borivali' , 
+        'medals' : 2
+        }  
+    ]
+print(d)
+
+# Question : How to create array of (integer's/number)
+h=[33,44,22]
+print(h)
+
+# Question : How to override variable and print
+o1 = 100
+print(o1)
+o1 = 200 
+print(o1) #here o1 got overriden by new value
+
