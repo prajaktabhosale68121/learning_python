033ecf7f8e7ceefbac89d33a603b9cbf39ec2e1c
Prajakta Bhosale <46363371+prajaktabhosale68121@users.noreply.github.com>
2019-01-07 22:49:49 +0530

    Create Loop.py
    loop learning

---
+++ Loop.py
@@ -0,0 +1,12 @@
+fruitsList = ["apple", "banana", "cherry"]
+
+
+def myFormula(yooo):
+    add = yooo + ' hello '
+    subract = add + ' hiee '
+    khanaKhaKeJana = subract + ' hmm '
+    print(khanaKhaKeJana)
+    
+for item in fruitsList:
+    myFormula(item)
+    
