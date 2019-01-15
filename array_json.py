4885cf147c2acb04d7fa1424875ef59e0c4c4207
Prajakta Bhosale <46363371+prajaktabhosale68121@users.noreply.github.com>
2019-01-15 21:36:08 +0530

    Update array_json.py

--- array_json.py
+++ array_json.py
@@ -32,3 +32,31 @@ for item in patients:
 print(counter)        
         
 
+from data import *
+
+patientList = patient
+
+# Q1 - get data of person whose first name is Georgie
+# for item in patient:
+#      if item['first_name'] =='Georgie':
+#         # print(item)
+         
+# Q2 -  how many people have phd as highest qualification
+# counter = 0
+# for item in patient:
+#     if item['Qualification'] == 'phd':
+#         counter = counter +1
+        
+# print(counter)  
+
+# Q3 - update salary to 60000 whose first name is Feodor 
+
+for sawt34tr4 in patient:
+    if sawt34tr4['first_name'] == 'Feodor':
+        sawt34tr4['salary'] = 60000
+        print(sawt34tr4)
+
+        
+        
+
+
