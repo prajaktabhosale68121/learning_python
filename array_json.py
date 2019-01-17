29714e58fdf6b69f3d921406334ac2a038b324ea
Prajakta Bhosale <46363371+prajaktabhosale68121@users.noreply.github.com>
2019-01-17 21:05:03 +0530

    Update array_json.py

--- array_json.py
+++ array_json.py
@@ -56,6 +56,36 @@ for sawt34tr4 in patient:
         sawt34tr4['salary'] = 60000
         print(sawt34tr4)
 
+  # Q4 - How many people are not married
+counter = 0      
+for item in patient:
+    if item['married'] == False:
+        counter = counter +1
+        
+# print(counter) 
+
+# Q6 - How many people work at boston_dynamics
+many = 0
+for item in patient:
+    if item['company'] == 'boston_dynamics':
+        many = many +1
+
+# print(many)
+
+
+# Q5 - A person With first name Brok was detected with coriander_allergy on 1 jan 2019 with severity level
+# As medium.So please update his allergies records with this newly detected
+
+for item in patient:
+     if item['first_name'] == 'Brok':
+        # print(item)
+        h = item['allergies']
+        print(h)
+        
+        item['allergies'] = [{'name':'coriander_allergy','severity_level':'medium','detection_date':'2019-01-01'}]
+        
+        print(item['allergies'])
+      
         
         
 
